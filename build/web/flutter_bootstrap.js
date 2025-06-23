67a82891bcc0c7a64fa4769106baa56b566a41fe
mjmichaelware <michaelware433@yahoo.com>
2025-06-23 02:00:20 +0000

    k

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4130363160"
+    serviceWorkerVersion: "891388530"
   }
 });
