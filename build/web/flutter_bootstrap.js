f976cc4d46f6c811d3cbcb1b1d3a93859ac3a86c
mjmichaelware <michaelware433@yahoo.com>
2025-06-23 13:19:21 +0000

    Initial Flutter web app

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "350426368"
+    serviceWorkerVersion: "1580686316"
   }
 });
